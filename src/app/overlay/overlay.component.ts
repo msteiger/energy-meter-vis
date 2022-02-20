72788108955bc4de8d082518d6efda696f00d2bb
Martin Steiger <git@martin-steiger.de>
2022-02-20 12:10:20 +0100

    Experimental conversion

--- src/app/overlay/overlay.component.ts
+++ src/app/overlay/overlay.component.ts
@@ -1,18 +1,26 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChartViewComponent } from '../chart-view/chart-view.component';
 
 @Component({
   selector: 'app-overlay',
   templateUrl: './overlay.component.html',
   styleUrls: ['./overlay.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
-export class OverlayComponent implements OnInit {
+export class OverlayComponent implements OnInit, AfterViewInit {
+
+  @Input('target')
+  target!: ElementRef<ChartViewComponent>;
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  @Input('text')
-  text: string = '';
+  ngAfterViewInit(): void {
+    const rect = this.target;
+    console.log("TARGET: " + JSON.stringify(rect));
+  }
+
 }
